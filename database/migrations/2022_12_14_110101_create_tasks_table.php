11823785f83c2b7a8ee5fe988c4f4d620100a51b
Ваше Имя <you@example.com>
2022-12-14 17:12:52 +0600

    create a file for database, change db connection to our database and add new 2 components in migration

--- database/migrations/2022_12_14_110101_create_tasks_table.php
+++ database/migrations/2022_12_14_110101_create_tasks_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('task');
+            $table->string('status')->default('new');
             $table->timestamps();
         });
     }
