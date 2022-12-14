0a37b259729df72e09754e5d40312e0fa6ad06ca
Ваше Имя <you@example.com>
2022-12-14 17:21:57 +0600

    create TasksController and add some actions for for further implementation

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function addTask()
+    {
+
+    }
+
+    public function statusInProgress()
+    {
+
+    }
+
+    public function statusDone()
+    {
+
+    }
+
+    public function deleteTask()
+    {
+
+    }
+
+    public function deleteAllTask()
+    {
+
+    }
+
+}
