5a0a611601dfa6ea551abddef9a3dafa7999b2cc
Ваше Имя <you@example.com>
2022-12-18 16:39:17 +0600

    add fillable property and create a 3 constants for actions status

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,12 +1,21 @@
 @extends('main.base')
 @section('content')
-    <div class="container mt-5">
+    <div class="container mt-5 bg-warning">
         @if(session('message'))
             <div class="alert alert-primary" role="alert">
                 {{ session('message') }}
             </div>
         @endif
         <h3 class="text-center mb-5 text-success">All Tasks</h3>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         <div class="row">
             <table class="table table-striped text-success">
                 <thead>
@@ -29,9 +38,16 @@
                 </tbody>
             </table>
             <div class="container">
+                <h2 class="text-success text-center mt-3">Add a new task</h2>
                 <div class="row">
                     <form action="{{action([\App\Http\Controllers\TasksController::class, 'addTask'])}}" method="POST">
-                        {{csrf_field()}}
+                        @csrf
+                        <div class="form-group">
+                            <input class="form-control" type="text" id="task" name="task" placeholder="To do.."/>
+                        </div>
+                        <div class="mt-3 text-center">
+                            <button type="submit" class="btn btn-primary">Add</button>
+                        </div>
                     </form>
                 </div>
             </div>
